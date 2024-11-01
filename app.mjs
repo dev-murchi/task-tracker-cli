c8578fecd5f03783f193c340842e9e201a34dfce
Murat ÇAY <murtcay@gmail.com>
2024-11-01 18:01:55 +0300

    list tasks

---
+++ app.mjs
@@ -0,0 +1,26 @@
+import { getTasks } from "./task-manager.mjs";
+
+const args = process.argv.splice(2);
+
+function runApp() {
+  if (args.length === 0) {
+    console.error('Error: No command provided. Use: list, add, update, delete, or mark.');
+    return;
+  }
+
+  const cmd = args[0];
+
+  if (cmd === 'list') {
+    const tasks = getTasks();
+    if (tasks.length === 0) {
+      console.log('No tasks available.');
+    }
+    tasks.forEach(task => console.log(`[${task.id}] - ${task.description} (${task.status})`));
+  }
+  else {
+    console.error('Error: Unknown command. Please use: list, create, delete, update, or status.');
+  }
+
+}
+
+runApp();
---
+++ task-manager.mjs
@@ -0,0 +1,16 @@
+const tasks = [
+  {
+    id: 1,
+    description: 'first task',
+    status: 'todo',
+  },
+  {
+    id: 2,
+    description: 'second task',
+    status: 'todo',
+  }
+];
+
+export function getTasks() {
+  return tasks;
+}
