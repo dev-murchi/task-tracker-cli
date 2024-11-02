347c53c030ed72f896637b769175aab5f5194d4d
Murat ÇAY <murtcay@gmail.com>
2024-11-02 14:47:02 +0300

    tests for updating the task status

--- __tests__/task-manager.test.mjs
+++ __tests__/task-manager.test.mjs
@@ -12,7 +12,7 @@ jest.unstable_mockModule('node:fs', () => ({
 }));
 
 const { writeFileSync, readFileSync, existsSync } = await import('node:fs');
-const { addTask, getTasks, updateTask } = await import('../task-manager.mjs');
+const taskManager = await import('../task-manager.mjs');
 
 describe('Task management', () => {
   beforeEach(() => {
@@ -24,7 +24,7 @@ describe('Task management', () => {
   test('should create and save a new task', () => {
     const description = 'Test task 1';
 
-    addTask(description);
+    taskManager.addTask(description);
 
     expect(testTasks).toHaveLength(1);
     expect(testTasks[0]).toEqual({
@@ -38,21 +38,21 @@ describe('Task management', () => {
 
   test('should not create a task with an empty description', () => {
     const consoleSpy = jest.spyOn(console, 'error');
-    addTask('');
+    taskManager.addTask('');
     expect(testTasks).toHaveLength(0);
     expect(consoleSpy).toHaveBeenCalledWith('Error: Please provide a valid task description. Description cannot be empty.');
     consoleSpy.mockRestore();
   });
 
   test('should list tasks correctly', async () => {
-    const tasks = getTasks();
+    const tasks = taskManager.getTasks();
     expect(tasks).toHaveLength(0);
     expect(tasks).toStrictEqual(testTasks);
 
-    addTask('First Test Task');
-    addTask('Second Test Task');
+    taskManager.addTask('First Test Task');
+    taskManager.addTask('Second Test Task');
 
-    const newTasks = getTasks();
+    const newTasks = taskManager.getTasks();
     expect(newTasks).toHaveLength(2);
     expect(newTasks).toStrictEqual(testTasks);
     expect(newTasks[0]).toEqual({
@@ -73,28 +73,27 @@ describe('Task management', () => {
   });
 
   test('should update a task description', () => {
-    addTask('Test Task 1');
+    taskManager.addTask('Test Task 1');
     expect(testTasks[0].id).toEqual(1);
     expect(testTasks[0].description).toEqual('Test Task 1');
     expect(testTasks[0].status).toEqual('todo');
     expect(testTasks[0].createdAt).toEqual(testTasks[0].updatedAt);
 
-    updateTask(1, 'Updated Task');
+    taskManager.updateTask(1, 'Updated Task');
     expect(testTasks[0].id).toEqual(1);
     expect(testTasks[0].description).toEqual('Updated Task');
     expect(testTasks[0].status).toEqual('todo');
-    expect(testTasks[0].createdAt).not.toEqual(testTasks[0].updatedAt);
     expect(new Date(testTasks[0].createdAt).getTime()).toBeLessThan(new Date(testTasks[0].updatedAt).getTime());
   });
 
   test('should not update a non-existent task', () => {
     const consoleLogSpy = jest.spyOn(console, 'log');
     const consoleErrorSpy = jest.spyOn(console, 'error');
-    updateTask(1, 'Updated Task');
+    taskManager.updateTask(1, 'Updated Task');
     expect(testTasks).toHaveLength(0);
     expect(consoleLogSpy).toHaveBeenCalledWith('Task [ID: 1] is not found.');
 
-    updateTask('invalidId', 'Updated Task');
+    taskManager.updateTask('invalidId', 'Updated Task');
     expect(testTasks).toHaveLength(0);
     expect(consoleErrorSpy).toHaveBeenCalledWith('Error: Please provide a valid task ID.');
 
@@ -104,14 +103,102 @@ describe('Task management', () => {
 
   test('should not update a task with an empty description', () => {
     const consoleSpy = jest.spyOn(console, 'error');
-    addTask('Test Task');
-    updateTask(1, '');
+    taskManager.addTask('Test Task');
+    taskManager.updateTask(1, '');
     expect(testTasks).toHaveLength(1);
     expect(testTasks[0].id).toEqual(1);
     expect(testTasks[0].description).toEqual('Test Task');
     expect(testTasks[0].status).toEqual('todo');
     expect(testTasks[0].createdAt).toEqual(testTasks[0].updatedAt);
     expect(consoleSpy).toHaveBeenCalledWith('Error: Please provide a valid task description. Description cannot be empty.');
+    consoleSpy.mockRestore();
+  });
+
+  test('should change the status of a task as (done)', () => {
+    taskManager.addTask('Test Task');
+    expect(testTasks).toHaveLength(1);
+    expect(testTasks[0]).toEqual({
+      id: 1,
+      description: 'Test Task',
+      status: 'todo',
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+    expect(testTasks[0].createdAt).toEqual(testTasks[0].updatedAt);
+
+    taskManager.markTaskStatus(1, 'done');
+    expect(testTasks[0]).toEqual({
+      id: 1,
+      description: 'Test Task',
+      status: 'done',
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+    expect(new Date(testTasks[0].createdAt).getTime()).toBeLessThan(new Date(testTasks[0].updatedAt).getTime());
+  });
+
+  test('should change the status of a task as (in-progress)', () => {
+    taskManager.addTask('Test Task');
+    expect(testTasks).toHaveLength(1);
+    expect(testTasks[0]).toEqual({
+      id: 1,
+      description: 'Test Task',
+      status: 'todo',
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+    expect(testTasks[0].createdAt).toEqual(testTasks[0].updatedAt);
+
+    taskManager.markTaskStatus(1, 'in-progress');
+    expect(testTasks[0]).toEqual({
+      id: 1,
+      description: 'Test Task',
+      status: 'in-progress',
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+    expect(new Date(testTasks[0].createdAt).getTime()).toBeLessThan(new Date(testTasks[0].updatedAt).getTime());
+  });
+
+  test('should not change the status of a non-existent task', () => {
+    const consoleLogSpy = jest.spyOn(console, 'log');
+    expect(testTasks).toHaveLength(0);
+    taskManager.markTaskStatus(1, 'todo');
+    expect(consoleLogSpy).toHaveBeenCalledWith('Task [ID: 1] is not found.');
+    consoleLogSpy.mockRestore();
+
+    const consoleErrorSpy = jest.spyOn(console, 'error');
+    taskManager.markTaskStatus('invalidId', 'todo');
+    expect(consoleErrorSpy).toHaveBeenCalledWith('Error: Please provide a valid task ID.');
+    consoleErrorSpy.mockRestore();
+  });
+
+  test('should not change the status of a task with ivalid value', () => {
+    taskManager.addTask('Test Task');
+    expect(testTasks).toHaveLength(1);
+    expect(testTasks[0]).toEqual({
+      id: 1,
+      description: 'Test Task',
+      status: 'todo',
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+    expect(testTasks[0].createdAt).toEqual(testTasks[0].updatedAt);
+
+    const consoleSpy = jest.spyOn(console, 'error');
+
+    taskManager.markTaskStatus(1, 'test');
+
+    expect(testTasks[0]).toEqual({
+      id: 1,
+      description: 'Test Task',
+      status: 'todo',
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+    expect(testTasks[0].createdAt).toEqual(testTasks[0].updatedAt);
+    expect(consoleSpy).toHaveBeenCalledWith('Error: Please provide a valid status (todo, in-progress, done).');
+    consoleSpy.mockRestore();
   });
 });
 
