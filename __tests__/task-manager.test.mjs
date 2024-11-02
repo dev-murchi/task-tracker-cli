3da623ccd7bd5b8f3d40a44cbf3322dc237ce6d7
Murat ÇAY <murtcay@gmail.com>
2024-11-02 14:02:03 +0300

    tests for updating task description

--- __tests__/task-manager.test.mjs
+++ __tests__/task-manager.test.mjs
@@ -1,4 +1,4 @@
-import { jest } from '@jest/globals';
+import { expect, jest } from '@jest/globals';
 
 let testTasks = [];
 let newTaskId = 1;
@@ -12,7 +12,7 @@ jest.unstable_mockModule('node:fs', () => ({
 }));
 
 const { writeFileSync, readFileSync, existsSync } = await import('node:fs');
-const { addTask, getTasks } = await import('../task-manager.mjs');
+const { addTask, getTasks, updateTask } = await import('../task-manager.mjs');
 
 describe('Task management', () => {
   beforeEach(() => {
@@ -70,6 +70,44 @@ describe('Task management', () => {
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
     });
+  });
+
+  test('should update a task description', () => {
+    addTask('Test Task 1');
+    expect(testTasks[0].id).toEqual(1);
+    expect(testTasks[0].description).toEqual('Test Task 1');
+    expect(testTasks[0].status).toEqual('todo');
+
+    updateTask(1, 'Updated Task');
+    expect(testTasks[0].id).toEqual(1);
+    expect(testTasks[0].description).toEqual('Updated Task');
+    expect(testTasks[0].status).toEqual('todo');
+  });
+
+  test('should not update a non-existent task', () => {
+    const consoleLogSpy = jest.spyOn(console, 'log');
+    const consoleErrorSpy = jest.spyOn(console, 'error');
+    updateTask(1, 'Updated Task');
+    expect(testTasks).toHaveLength(0);
+    expect(consoleLogSpy).toHaveBeenCalledWith('Task [ID: 1] is not found.');
+
+    updateTask('invalidId', 'Updated Task');
+    expect(testTasks).toHaveLength(0);
+    expect(consoleErrorSpy).toHaveBeenCalledWith('Error: Please provide a valid task ID.');
+
+    consoleLogSpy.mockRestore();
+    consoleErrorSpy.mockRestore();
+  });
+
+  test('should not update a task with an empty description', () => {
+    const consoleSpy = jest.spyOn(console, 'error');
+    addTask('Test Task');
+    updateTask(1, '');
+    expect(testTasks).toHaveLength(1);
+    expect(testTasks[0].id).toEqual(1);
+    expect(testTasks[0].description).toEqual('Test Task');
+    expect(testTasks[0].status).toEqual('todo');
+    expect(consoleSpy).toHaveBeenCalledWith('Error: Please provide a valid task description. Description cannot be empty.');
   });
 });
 
