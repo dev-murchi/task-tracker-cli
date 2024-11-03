a029a8777b4426f821f440f3668d6c5eb65311a6
Murat ÇAY <murtcay@gmail.com>
2024-11-03 15:10:28 +0300

    use single try catch block

--- app.mjs
+++ app.mjs
@@ -3,62 +3,44 @@ import { addTask, deleteTask, getTasks, markTaskStatus, updateTask } from "./tas
 const args = process.argv.splice(2);
 
 function runApp() {
-  if (args.length === 0) {
-    console.error('Error: No command provided. Use: list, add, update, delete, or mark.');
-    return;
-  }
+  try {
+    if (args.length === 0) {
+      throw new Error('Error: No command provided. Use: list, add, update, delete, or mark.');
+    }
 
-  const cmd = args[0];
+    const cmd = args[0];
 
-  if (cmd === 'list') {
-    try {
+    if (cmd === 'list') {
       const tasks = getTasks();
       if (tasks.length === 0) {
         console.log('No tasks available.');
       }
       tasks.forEach(task => console.log(`[${task.id}] - ${task.description} (${task.status})`));
-    } catch (error) {
-      console.error(error.message);
     }
-  }
-  else if (cmd === 'add') {
-    try {
+    else if (cmd === 'add') {
       const description = args.slice(1).join(' ');
       addTask(description);
-    } catch (error) {
-      console.error(error.message);
     }
-  }
-  else if (cmd === 'update') {
-    try {
+    else if (cmd === 'update') {
       const id = args[1];
       const description = args.slice(2).join(' ');
       updateTask(id, description);
-    } catch (error) {
-      console.error(error.message);
     }
-  }
-  else if (cmd === 'delete') {
-    try {
+    else if (cmd === 'delete') {
       const id = args[1];
       deleteTask(id);
-    } catch (error) {
-      console.error(error.message);
     }
-  }
-  else if (cmd === 'mark') {
-    try {
+    else if (cmd === 'mark') {
       const id = args[1];
       const status = args[2];
       markTaskStatus(id, status);
-    } catch (error) {
-      console.error(error.message);
     }
+    else {
+      throw new Error('Error: Unknown command. Please use: list, add, delete, update, or status.');
+    }
+  } catch (error) {
+    console.error(error.message);
   }
-  else {
-    console.error('Error: Unknown command. Please use: list, add, delete, update, or status.');
-  }
-
 }
 
 runApp();
